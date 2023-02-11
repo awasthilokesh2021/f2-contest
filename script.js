9b9f1309572d44a54bb8e746310c0e665f8e9112
awasthilokesh2021 <115865786+awasthilokesh2021@users.noreply.github.com>
2023-02-11 21:04:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,17 +6,35 @@ let arr = [
   { id: 3, name: "karen", age: "19", marks: 35 },
 ];
 
-function PrintStudentswithMap() {
-  //Write your code here , just console.log
+arr.map(function(element,arr){
+  for(var i=0;i<arr.length;i++){
+    if(arr[i]>50){
+      return element;
+    }
+  }
+})
+
+function PrintStudentswithMap(arr){
+  var student = arr.map(function(marks)
+  {
+    while(arr[i]>50){
+      return marks;
+    }
+  })
+   console.log(student);
 }
 
+var student = student.
 function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
 }
 
-function addData() {
-  //Write your code here, just console.log
+
+function addData(arr) {
+  var s = []
+  s.push({id:4,name:"susan",age:"20",marks:45})
 }
+console.log(s);
+
 
 function removeFailedStudent() {
   //Write your code here, just console.log
